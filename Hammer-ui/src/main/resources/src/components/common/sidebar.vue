3a066a1bca05112450679f08bb1e2ce43fbce7c9
Kerbores <Kerbores@gmail.com>
2017-03-17 17:30:39 +0800

    add:基于VUE的前端页面

---
+++ Hammer-ui/src/main/resources/src/components/common/sidebar.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="sidebar">
+        <el-menu :default-active="onRoutes" class="el-menu-vertical-demo" theme="dark" unique-opened router>
+            <el-menu-item index="main">
+                <i class="el-icon-setting"></i>配置中心概述
+            </el-menu-item>
+            <el-submenu index="2">
+                <template slot="title"><i class="el-icon-menu"></i>通用数据</template>
+                <el-menu-item index="env">环境管理</el-menu-item>
+                <el-menu-item index="common">通用配置</el-menu-item>
+            </el-submenu>
+            <el-submenu index="3">
+                <template slot="title"><i class="el-icon-date"></i>项目配置</template>
+                <el-submenu index="kkk">
+                    <template slot="title"><i class="el-icon-setting"></i>项目管理</template>
+                    <el-menu-item index="basecharts">项目列表</el-menu-item>
+                    <el-menu-item index="mixcharts">数据源</el-menu-item>
+                    <el-menu-item index="mixcharts">应用状态</el-menu-item>
+                </el-submenu>
+                <!--<el-menu-item index="vueeditor">编辑器</el-menu-item>-->
+                <!--<el-menu-item index="markdown">markdown</el-menu-item>-->
+                <!--<el-menu-item index="upload">文件上传</el-menu-item>-->
+            </el-submenu>
+            <el-submenu index="4">
+                <template slot="title"><i class="el-icon-star-on"></i>ACL</template>
+                <el-menu-item index="basecharts">用户管理</el-menu-item>
+                <el-menu-item index="mixcharts">角色管理</el-menu-item>
+                <el-menu-item index="mixcharts">权限管理</el-menu-item>
+            </el-submenu>
+            <el-submenu index="5">
+                <template slot="title"><i class="el-icon-setting"></i>监控</template>
+                <el-menu-item index="basecharts">系统监控</el-menu-item>
+                <el-menu-item index="dataSource">数据源</el-menu-item>
+                <el-menu-item index="mixcharts">应用状态</el-menu-item>
+            </el-submenu>
+        </el-menu>
+    </div>
+</template>
+<script>
+    export default {
+        computed:{
+            onRoutes(){
+                return this.$route.path.replace('/','');
+            }
+        }
+    }
+</script>
+<style scoped>
+    .sidebar{
+        display: block;
+        position: absolute;
+        width: 250px;
+        left: 0;
+        top: 70px;
+        bottom:0;
+        background: #2E363F;
+    }
+    .sidebar > ul {
+        height:100%;
+    }
+</style>
---
+++ Hammer-ui/src/main/resources/src/components/page/Login.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="login-wrap">
+        <div class="ms-title">Nutz-Hammer</div>
+        <div class="ms-login">
+            <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px" class="demo-ruleForm">
+                <el-form-item prop="username">
+                    <el-input v-model="ruleForm.username" :placeholder="rules.username[0].message"></el-input>
+                </el-form-item>
+                <el-form-item prop="password">
+                    <el-input type="password" :placeholder="rules.password[0].message" v-model="ruleForm.password" @keyup.enter.native="submitForm('ruleForm')"></el-input>
+                </el-form-item>
+                <div class="login-btn">
+                    <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
+                </div>
+                <p style="font-size:12px;line-height:30px;color:#999;">Tips : 用户密码不能随便填。</p>
+            </el-form>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function(){
+            return {
+                ruleForm: {
+                    username: 'kerbores',
+                    password: '742425'
+                },
+                rules: {
+                    username: [
+                        { required: true, message: '请输入用户名', trigger: 'blur' }
+                    ],
+                    password: [
+                        { required: true, message: '请输入密码', trigger: 'blur' }
+                    ]
+                }
+            }
+        },
+        methods: {
+            submitForm(formName) {
+                const self = this;
+                self.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        localStorage.setItem('ms_username',self.ruleForm.username);
+                        //这里做提交操作
+//                        this.get(contextPath + '/index',data=>{
+//                            console.log(data)
+//                        });
+                        self.$router.push('/main');
+                    } else {
+                        console.log('error submit!!');
+                        return false;
+                    }
+                });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .login-wrap{
+        position: relative;
+        width:100%;
+        height:100%;
+    }
+    .ms-title{
+        position: absolute;
+        top:50%;
+        width:100%;
+        margin-top: -230px;
+        text-align: center;
+        font-size:30px;
+        color: #fff;
+
+    }
+    .ms-login{
+        position: absolute;
+        left:50%;
+        top:50%;
+        width:300px;
+        height:160px;
+        margin:-150px 0 0 -190px;
+        padding:40px;
+        border-radius: 5px;
+        background: #fff;
+    }
+    .login-btn{
+        text-align: center;
+    }
+    .login-btn button{
+        width:100%;
+        height:36px;
+    }
+</style>
