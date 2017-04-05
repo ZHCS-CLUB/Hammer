2b1b076f035da4bb26b4c11284ea49e028d5fd50
Kerbores <Kerbores@gmail.com>
2017-04-05 21:57:12 +0800

    change:概述页面

--- Hammer-ui/src/main/resources/src/components/common/Sidebar.vue
+++ Hammer-ui/src/main/resources/src/components/common/Sidebar.vue
@@ -1,51 +1,66 @@
 <template>
     <div class="sidebar">
-        <el-menu :default-active="onRoutes" class="el-menu-vertical-demo" theme="dark" unique-opened router>
+        <el-menu :default-active="onRoutes"
+                 class="el-menu-vertical-demo"
+                 theme="dark"
+                 unique-opened
+                 router>
             <el-menu-item index="readme">
-                <i class="el-icon-setting"></i>项目自述
+                <i class="el-icon- fa fa-tachometer"></i>项目自述
             </el-menu-item>
             <el-submenu index="2">
-                <template slot="title"><i class="el-icon-menu"></i>表格</template>
-                <el-menu-item index="basetable">基础表格</el-menu-item>
-                <el-menu-item index="vuetable">Vue表格组件</el-menu-item>
+                <template slot="title"><i class="el-icon- fa fa-users"></i> 访问控制</template>
+                <el-menu-item index="basetable"><i class="fa fa-user"></i> 用户管理</el-menu-item>
+                <el-menu-item index="vuetable"><i class="fa fa-lock"></i> 角色管理</el-menu-item>
+                <el-menu-item index="vuetable"><i class="fa fa-eye"></i> 权限管理</el-menu-item>
             </el-submenu>
             <el-submenu index="3">
-                <template slot="title"><i class="el-icon-date"></i>表单</template>
-                <el-menu-item index="baseform">基本表单</el-menu-item>
-                <el-menu-item index="vueeditor">编辑器</el-menu-item>
-                <el-menu-item index="markdown">markdown</el-menu-item>
-                <el-menu-item index="upload">文件上传</el-menu-item>
+                <template slot="title"><i class="el-icon- fa fa-book"></i>码本管理</template>
+                <el-menu-item index="baseform"><i class="fa fa-object-group"></i> 码本分组</el-menu-item>
+                <el-menu-item index="vueeditor"><i class="fa fa-cubes"></i> 码本数据</el-menu-item>
             </el-submenu>
             <el-submenu index="4">
-                <template slot="title"><i class="el-icon-star-on"></i>图表</template>
-                <el-menu-item index="basecharts">基础图表</el-menu-item>
-                <el-menu-item index="mixcharts">混合图表</el-menu-item>
+                <template slot="title"><i class="el-icon- fa fa-cogs"></i>配置管理</template>
+                <el-menu-item index="basecharts"><i class="fa fa-cog"></i> 配置列表</el-menu-item>
+            </el-submenu>
+            <el-submenu index="5">
+                <template slot="title"><i class="el-icon- fa fa-building-o"></i>组织结构</template>
+                <el-menu-item index="basecharts"><i class="fa fa-bank"></i> 机构管理</el-menu-item>
+                <el-menu-item index="basecharts"><i class="fa fa-chrome"></i> 部门管理</el-menu-item>
+            </el-submenu>
+            <el-submenu index="6">
+                <template slot="title"><i class="el-icon- fa fa-camera"></i>监控面板</template>
+                <el-menu-item index="basecharts"><i class="fa fa-database"></i> Druid监控</el-menu-item>
+                <el-menu-item index="basecharts"><i class="fa fa-server"></i> 运行环境</el-menu-item>
+                <el-menu-item index="basecharts"><i class="fa fa-history"></i> 登录日志</el-menu-item>
+                <el-menu-item index="basecharts"><i class="fa fa-tags"></i> 操作日志</el-menu-item>
             </el-submenu>
         </el-menu>
     </div>
 </template>
 
 <script>
-    export default {
-        computed:{
-            onRoutes(){
-                return this.$route.path.replace('/','');
-            }
+export default {
+    computed: {
+        onRoutes() {
+            return this.$route.path.replace('/', '');
         }
     }
+}
 </script>
 
 <style scoped>
-    .sidebar{
-        display: block;
-        position: absolute;
-        width: 250px;
-        left: 0;
-        top: 70px;
-        bottom:0;
-        background: #2E363F;
-    }
-    .sidebar > ul {
-        height:100%;
-    }
+.sidebar {
+    display: block;
+    position: absolute;
+    width: 250px;
+    left: 0;
+    top: 70px;
+    bottom: 0;
+    background: #2E363F;
+}
+
+.sidebar>ul {
+    height: 100%;
+}
 </style>
