3cb3c0dde295e195c5437f732b4939936659aba6
Kerbores <Kerbores@gmail.com>
2017-04-05 12:00:42 +0800

    change:一些测试接口&依赖关系

--- Hammer-rest/src/main/java/club/zhcs/hammer/module/MainModule.java
+++ Hammer-rest/src/main/java/club/zhcs/hammer/module/MainModule.java
@@ -2,13 +2,20 @@ package club.zhcs.hammer.module;
 
 import org.nutz.dao.Dao;
 import org.nutz.ioc.loader.annotation.Inject;
+import org.nutz.lang.random.R;
+import org.nutz.lang.util.NutMap;
+import org.nutz.mvc.View;
+import org.nutz.mvc.adaptor.JsonAdaptor;
+import org.nutz.mvc.annotation.AdaptBy;
 import org.nutz.mvc.annotation.At;
 import org.nutz.mvc.annotation.Fail;
 import org.nutz.mvc.annotation.IocBy;
 import org.nutz.mvc.annotation.Modules;
 import org.nutz.mvc.annotation.Ok;
+import org.nutz.mvc.annotation.Param;
 import org.nutz.mvc.ioc.provider.ComboIocProvider;
 
+import club.zhcs.titans.nutz.captcha.JPEGView;
 import club.zhcs.titans.nutz.module.base.AbstractBaseModule;
 import club.zhcs.titans.utils.db.Result;
 
@@ -16,9 +23,10 @@ import club.zhcs.titans.utils.db.Result;
  * @author kerbores kerbores@gmail.com
  *
  */
+@Modules
 @Ok("json")
 @Fail("http:500")
-@Modules
+@AdaptBy(type = JsonAdaptor.class)
 @IocBy(type = ComboIocProvider.class, args = { "*anno", "club.zhcs", "*tx", "*js", "ioc" })
 public class MainModule extends AbstractBaseModule {
 
@@ -27,12 +35,28 @@ public class MainModule extends AbstractBaseModule {
 
 	@At("/")
 	public Result index() {
-		return Result.success();
+		return R.random(0, 10) > 5 ? Result.success() : Result.fail("test");
+	}
+
+	@At
+	public Result post(@Param("id") int id, @Param("name") String name) {
+		return Result.success(NutMap.NEW().addv("id", id).addv("name", name));
+	}
+
+	@At
+	@AdaptBy(type = JsonAdaptor.class)
+	public Result postBody(NutMap data) {
+		return Result.success(data);
 	}
 
 	@At
 	public Result db() {
 		return Result.success().addData("db", dao.meta());
+	}
+
+	@At
+	public View captcha() {
+		return new JPEGView(null);
 	}
 
 }
