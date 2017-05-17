c4cfd2a8259d7e4edb32d84bfe58cd182155394c
王贵源 <guiyuan.wang-N@msxf.com>
2017-05-17 18:05:44 +0800

    fix:码本搞定

--- Hammer-ui/src/main/resources/src/components/page/Codebook.vue
+++ Hammer-ui/src/main/resources/src/components/page/Codebook.vue
@@ -25,38 +25,10 @@
         <el-table :data="pager.entities" border style="width: 100%">
             <el-table-tree-column 
             :remote="remote"
-            :expand-all="!!1"
             file-icon="icon icon-file" 
             folder-icon="icon icon-folder" 
+            parentKey="parentId"
             prop="id" label="ID"></el-table-tree-column>
-            <el-table-column prop="name" label="Key">
-            </el-table-column>
-            <el-table-column prop="value" label="Value">
-            </el-table-column>
-            <el-table-column label="操作">
-                <template scope="scope">
-                    <el-dropdown>
-                        <el-button type="primary">
-                            操作
-                            <i class="el-icon-caret-bottom el-icon--right"></i>
-                        </el-button>
-                        <el-dropdown-menu slot="dropdown">
-                            <el-dropdown-item>
-                                <div @click="handleEdit(scope.$index,scope.row)">
-                                    <i class="fa fa-edit"></i> 编辑码本</div>
-                            </el-dropdown-item>
-                            <el-dropdown-item v-show="!scope.row.installed">
-                                <div @click="handleDelete(scope.$index,scope.row)">
-                                    <i class="fa fa-trash-o"></i> 删除码本</div>
-                            </el-dropdown-item>
-                        </el-dropdown-menu>
-                    </el-dropdown>
-                </template>
-            </el-table-column>
-        </el-table>
-        <el-table :data="pager.entities" border style="width: 100%">
-            <el-table-column prop="id" label="ID" sortable>
-            </el-table-column>
             <el-table-column prop="name" label="Key">
             </el-table-column>
             <el-table-column prop="value" label="Value">
@@ -167,7 +139,13 @@ export default {
     methods: {
         remote(row,callback){
             this.get('/codebook/sub/'+row.id,result=>{
-                callback(result.data.codes);
+                const data = [];
+                 result.data.codes.forEach(item=>{
+                    item.children = [{}];
+                     data.push(item)
+                 })
+                // row.children = data;
+                callback(data);
             })
         },
         loadGroups() {
