c09f5a9537d42eb517fe5e9642722273e2d0fa77
王贵源 <guiyuan.wang-N@msxf.com>
2017-05-24 17:51:35 +0800

    fix:头像处理

--- Hammer-ui/src/main/resources/src/components/page/Login.vue
+++ Hammer-ui/src/main/resources/src/components/page/Login.vue
@@ -64,7 +64,7 @@ export default {
                         localStorage.setItem('roles', JSON.stringify(data.data.roles));
                         localStorage.setItem('permissions', JSON.stringify(data.data.permissions));
                         localStorage.setItem('loginUser', data.data.loginUser.realName || data.data.loginUser.name);
-                        localStorage.setItem('head', data.data.loginUser.headKey || '../../../static/img/img.jpg');
+                        localStorage.setItem('head', data.data.loginUser.headKey ? 'http://7xlb6l.com1.z0.glb.clouddn.com/'+data.data.loginUser.headKey : '../../../static/img/img.jpg');
                         self.$router.push('/readme');
                     }, result => {
                         this.$message({
